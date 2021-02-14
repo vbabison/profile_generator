8bef5f95c10cece45c9a1e4f0baa9fef3c774fbf
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-14 03:50:01 -0500

    Callback hell fix

--- survey.js
+++ survey.js
@@ -4,20 +4,71 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-rl.question('What\'s your name? Nicknames are also acceptable :)', (name) => {
-  rl.question('What\'s an activity you like doing? ', (activity) => {
-    rl.question('What do you listen to while doing that? ', (music) => {
-      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (meal) => {
-        rl.question('What\'s your favourite thing to eat for that meal? ', (favMeal) => {
-          rl.question('Which sport is your absolute favourite? ', (sport) => {
-            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
-              console.log(`Hi, my name is ${name}. I like to listen to ${music} while ${activity}. I love having ${favMeal} during ${meal}. I can never get over ${sport}, as it's my favourite sport! My superpower is, ${superpower}.`);
-              rl.close();
-            });
-          });
-        });
-      });
+const question1 = () => {
+  return new Promise((resolve) => {
+    rl.question('What\'s your name? Nicknames are also acceptable :)', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
     });
   });
-});
+};
+const question2 = () => {
+  return new Promise((resolve) => {
+    rl.question('What\'s an activity you like doing? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const question3 = () => {
+  return new Promise((resolve) => {
+    rl.question('What do you listen to while doing that? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const question4 = () => {
+  return new Promise((resolve) => {
+    rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const question5 = () => {
+  return new Promise((resolve) => {
+    rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const question6 = () => {
+  return new Promise((resolve) => {
+    rl.question('Which sport is your absolute favourite? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const question7 = () => {
+  return new Promise((resolve) => {
+    rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      resolve();
+    });
+  });
+};
+const main = async() => {
+  await question1();
+  await question2();
+  await question3();
+  await question4();
+  await question5();
+  await question6();
+  await question7();
+  rl.close();
+};
+
+main();
